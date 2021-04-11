c85923bba5c00d8784d5f9f24ddc4ba74165b790
antonivegaochoa <tonivegaochoa@gmail.com>
2021-04-11 13:14:43 -0400

    Finished

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -8,9 +8,52 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var text = ""
+    @State private var showAlert = false
+
+
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color.yellow.edgesIgnoringSafeArea(.all)
+            
+            VStack(spacing: 10) {
+                Text("Enter the secret code")
+                    .font(.title)
+                    .padding()
+                
+                ForEach(0 ..< 3) { row in
+                    HStack {
+                        ForEach(0 ..< 3) { col in
+                            let num = row * 3 + col + 1
+                            Button(action: {
+                                text += String(num)
+                                checkCode()
+                            }) {
+                                Image(systemName: "\(num).circle.fill")
+                                    .renderingMode(.original)
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(height: 50)
+                            }
+                        }
+                    }
+                }
+            }
+            
+            .alert(isPresented: $showAlert) {
+                Alert(title: Text("SUCCESS!"), message: Text("You entered the secret code!"), dismissButton: .default(Text("Dismiss")))
+            }
+        }
+    }
+    
+    func checkCode() {
+        if text.count == 4 {
+            if text == "1119" {
+                showAlert = true
+            }
+            text = ""
+        }
     }
 }
 
